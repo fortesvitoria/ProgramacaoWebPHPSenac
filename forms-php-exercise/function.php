7c379b01fa2357e8b922df914193aa13d6b6461d
Vitoria Fortes <fortesvitoria@gmail.com>
2025-09-29 14:49:22 -0300

    update form exercise

--- forms-php-exercise/function.php
+++ forms-php-exercise/function.php
@@ -16,18 +16,21 @@ $listaCursos = [
 
 if (isset($_POST['enviar']) && !empty($_POST['curso'])) {
     $cursoDigitado = trim($_POST['curso']);
-    $erro = "Curso \"{$cursoDigitado}\" não encontrado.";
 
     foreach ($listaCursos as $curso) {
         //strcasecmp compara se as strings são iguais, se sim, retorna 0, se for maior, retorna maior que zero, se menor, retorna menor que zero, utilizada para tratar upper and lowercase
         if (strcasecmp($cursoDigitado, $curso['curso']) == 0) {
             $_SESSION['cursoSelecionado'] = $curso['curso'];
             $_SESSION['valorCurso'] = $curso['valor'];
+            unset($_SESSION['erro']); //limpa  dados anteriores
             // Redirecionar
             header("Location: resultado.php");
-        } 
+            exit();
+        }
     }
-     $_SESSION['erro'] = $erro;
-            header("Location: resultado.php");
+    $_SESSION['erro'] = "Curso \"{$cursoDigitado}\" não encontrado.";
+    unset($_SESSION['cursoSelecionado']);//limpa  dados anteriores
+    unset($_SESION['valorCurso']);//limpa  dados anteriores
+    header("Location: resultado.php");
+    exit();
 }
-?>
